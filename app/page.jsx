999efd31574dc62e5f973c51138be0c65b5995eb
digimori <hanaorikuu@gmail.com>
2023-08-13 15:12:29 +0000

    Home page populated with basic intro text.

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { metadata } from './layout';
 
-const Home = () => {
+const Home = ({ metadata, description }) => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient'>AI Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Promptodo is an AI Open-source bluh bluh bluh Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Eum, id voluptatibus! Vel quisquam sed, ex incidunt ipsam aliquid commodi ipsa, 
+        aspernatur deserunt facere veniam asperiores quas, deleniti quibusdam quo molestias.</p>
+
+        {/* Feed goes here */}
+    </section>
   )
 }
 
