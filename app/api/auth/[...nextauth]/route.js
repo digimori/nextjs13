e7da8e443895da9c4ac693f566087a811919f60f
digimori <hanaorikuu@gmail.com>
2023-08-14 18:49:27 +0000

    Authentication process complete. MongoDB hooked to database.js and route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import User from '@models/user';
 
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
     providers: [
@@ -10,10 +12,35 @@ const handler = NextAuth({
       })
     ],
     async session({ session }) {
-
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      })
+      session.user.id = sessionUser._id.toString();
+      return session;
     },
     async signIn({ profile }) {
-        
+      try {
+        await connectToDB();
+
+        // Check if user already exists
+        const userExists = await User.findOne({
+          email: profile.email
+        });
+
+        // if not, create and save user to DB
+        if(!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          })
+        }
+        return true;
+
+      } catch (error) {
+        console.log('Unable to connect to DB');
+        return false;
+      }
     }
 })
 
