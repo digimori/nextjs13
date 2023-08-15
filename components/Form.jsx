e137b20f23da6ffb9398c025bb647102101268a1
digimori <hanaorikuu@gmail.com>
2023-08-15 18:21:49 +0000

    Create/Cancel buttons added to forms - Currently does not POST to database

--- components/Form.jsx
+++ components/Form.jsx
@@ -28,12 +28,20 @@ const Form = ({ type, post, setPost, submitting, handleSubmit}) => {
                 <span className='font-normal'>(#product, #webdevelopment, #idea)</span>
                 </span>
                 <input
-                value={post.ptag}
+                value={post.tag}
                 onChange={(e) => setPost({ ...post, tag: e.target.value})}
                 placeholder='Write your tags here...'
                 required
                 className='form_input' />
               </label>
+              <div className='flex-end mx-3 mb-5 gap-4'>
+              <Link href="/" className='text-gray-500 text-sm'>Cancel</Link>
+              <button type="submit"
+                      disabled={submitting}
+                      className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'>
+                {submitting ? `${type}...` : type}
+              </button>
+              </div>
             </form>
     </section>
   )
