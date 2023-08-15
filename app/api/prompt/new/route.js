e45c6ea331c6ed193a0d4810e15747c53c9486e1
digimori <hanaorikuu@gmail.com>
2023-08-15 18:50:50 +0000

    API hook started, Model schema for prompt created

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,12 @@
+import { connectToDB } from "@utils/database";
+
+
+export const POST = async (req) => {
+    const { userID, prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+    } catch (error) {
+        console.log(error)
+    }
+}
--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
+import { Router } from 'next/router';
 
 const CreatePrompt = () => {
     const [submitting, setSubmitting] = useState(false)
@@ -13,7 +14,26 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
-
+      e.preventDefault();
+      setSubmitting(true);
+      try {
+        const response = await fetch('/api/prompt/new',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post.prompt,
+            userID: session?.user.id,
+            tag: post.tag
+          })
+        })
+        if(response.ok) {
+          router.push('/')
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setSubmitting(false);
+      }
     }
 
   return (
