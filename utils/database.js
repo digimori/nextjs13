7ef0d9cc8aa1161a0c3a4c1457e63e8161ec196b
digimori <hanaorikuu@gmail.com>
2023-08-15 14:06:57 +0000

    Form added to create-prompt page.

--- utils/database.js
+++ utils/database.js
@@ -1,23 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let isConnected = false; // Track connection 
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
-    if(isConnected) {
-        console.log('MongoDB is already connected')
-        return;
-    }
+  mongoose.set('strictQuery', true);
 
-try {
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
     await mongoose.connect(process.env.MONGODB_URI, {
-        dbName: 'share_prompt',
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
+
     isConnected = true;
+
     console.log('MongoDB connected')
-} catch (error) {
-    console.log(error)
-}
+  } catch (error) {
+    console.log(error);
+  }
 }
