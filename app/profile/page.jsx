43c8d027d795bb1d741e5142e73045b65defd66b
digimori <hanaorikuu@gmail.com>
2023-08-22 12:49:18 +0000

    Profile app created - useEffect for fetching posts added to ProfilePage component in profile/page.jsx

---
+++ app/profile/page.jsx
@@ -0,0 +1,42 @@
+'use client'
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Profile from '@components/Profile';
+import PromptCard from '@components/PromptCard'
+
+const ProfilePage = () => {
+// Get the session
+const { data: session } = useSession();
+
+const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+      
+          setPosts(data)
+        }
+        if(session?.user.id) fetchPosts();
+      }, [])
+
+    const handleEdit = () => {
+
+    }
+    const handleDelete = async () => {
+
+    }
+
+  return (
+    <Profile name="My"
+             desc="Welcome to your Profile page"
+             data={posts}
+             handleEdit={handleEdit}
+             handleDelete={handleDelete} />
+  )
+}
+
+export default ProfilePage
